e3f94f95e73056d852c3aec17db7059e0e5f006d
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2026-01-12 11:50:05 +0100

    freE

--- src/app/components/about/AboutUsOnHomePage.tsx
+++ src/app/components/about/AboutUsOnHomePage.tsx
@@ -21,11 +21,11 @@ const AboutUsOnHomePage = () => {
           <div className='flex flex-col lg:flex-row px-5 lg:px-[60px] py-16 2xl:px-[120px] lg:py-36'>
               <div className='w-full lg:w-1/2'>
               <Image 
-                src="/assets/IMG_3865.png" 
+                src="/assets/Easybuild shorts.00_07_59_18.webp" 
                 alt='About us image' 
-                width={600}
-                height={400}
-                className='w-full h-full object-cover rounded-[15px]' 
+                width={1200}
+                height={600}
+                className='w-full h-full object-contain rounded-[15px]' 
               />
               </div>
 
